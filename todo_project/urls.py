9c097f590d25f4a9f3a8a8a8eb02047659ca4fa6
ThalesOfMiletus <thalesmartinscogo@gmail.com>
2024-07-29 18:09:23 -0300

    add pagina home

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -1,8 +1,10 @@
 # todo_project/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from todo.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls')),
+    path('', home, name='home'),
 ]
